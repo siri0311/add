557b180672e3031227cbadb07d3e16095f98ef36
Sirisha Avvaru <arsirisha03@gmail.com>
2024-03-15 11:20:53 +0530

    Add code for addition of two numbers

---
+++ add.c
@@ -0,0 +1,12 @@
+/**
+ * @file add.c
+ * @brief Implementation of the addition function.
+ */
+
+#include<stdio.h>
+#include"add.h"
+
+int add(int number1, int number2) 
+{
+    return number1 + number2;
+}
---
+++ add.h
@@ -0,0 +1,30 @@
+/**
+ * @file add.h
+ * @brief Header file for the addition function.
+ */
+
+#ifndef ADD_H
+#define ADD_H
+
+/**
+ * @brief Adds two integers.
+ *
+ * This function declaration specifies the signature of the add function.
+ * It takes two integers as input parameters and returns their sum.
+ *
+ * @param number1 The first integer operand.
+ * @param number2 The second integer operand.
+ * @return The sum of the two input integers.
+ *
+ * Example usage:
+ * @code
+ * #include "add.h"
+ * // ...
+ * int result = add(5, 3);
+ * // ...
+ * @endcode
+ */
+int add(int number1, int number2);
+
+#endif
+
